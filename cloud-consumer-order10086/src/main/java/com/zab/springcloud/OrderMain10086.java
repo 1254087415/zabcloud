7542632cafb1582367993e53f9039648f64fcb33
张海彬 <1254087415@qq.com>
2021-10-13 11:46:41 +0800

    IRule的配置和手写轮询负载均衡

--- cloud-consumer-order10086/src/main/java/com/zab/springcloud/OrderMain10086.java
+++ cloud-consumer-order10086/src/main/java/com/zab/springcloud/OrderMain10086.java
@@ -1,11 +1,14 @@
 package com.zab.springcloud;
 
+import com.zab.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain10086 {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderMain10086.class, args);
