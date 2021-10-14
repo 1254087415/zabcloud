e9db32426dd6bd7ff2438dd29face638f1ea7c7a
zab <1254087415@qq.com>
2021-10-14 23:38:26 +0800

    修改为Feign

--- cloud-consumer-feign-hystrix-order10086/src/main/java/com/zab/springcloud/OrderHystrixMain10086.java
+++ cloud-consumer-feign-hystrix-order10086/src/main/java/com/zab/springcloud/OrderHystrixMain10086.java
@@ -3,9 +3,10 @@ package com.zab.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableFeignClients
 public class OrderHystrixMain10086 {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderHystrixMain10086.class, args);
