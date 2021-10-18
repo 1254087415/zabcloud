e13ce642e5f58bfd8b4dbb58ef03c75c64f4cfb0
张海彬 <1254087415@qq.com>
2021-10-18 14:44:39 +0800

    添加服务降级代码

--- cloud-provider-hystrix-payment8001/src/main/java/com/zab/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/zab/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.zab.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -12,6 +14,9 @@ public class PaymentService {
 	}
 	
 	//失败
+	@HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+	})
 	public String paymentInfo_TimeOut(Integer id) {
 		int timeNumber = 3;
 		try {
@@ -20,5 +25,10 @@ public class PaymentService {
 			e.printStackTrace();
 		}
 		return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id：  " + id + "\t" + "呜呜呜" + " 耗时(秒)" + timeNumber;
+	}
+	
+	//兜底方法
+	public String paymentInfo_TimeOutHandler(Integer id){
+		return "线程池："+Thread.currentThread().getName()+"   系统繁忙, 请稍候再试  ,id：  "+id+"\t"+"哭了哇呜";
 	}
 }
